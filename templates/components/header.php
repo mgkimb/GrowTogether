dfde29e8815ccd02abc4d98a84164bcb12f1e628
mgkimb <98755684+mgkimb@users.noreply.github.com>
2023-09-23 01:48:16 +0900

    navbar 1st draft

---
+++ templates/components/header.php
@@ -0,0 +1,41 @@
+    <link rel="stylesheet" href="../assets/css/header.css">
+
+    <link href='https://fonts.googleapis.com/css?family=Nunito' rel='stylesheet'>
+
+</head>
+
+<body>
+    <header class="navbar">
+        <!-- Upper Navbar -->
+        <div class="upper-navbar">
+            <div class="container">
+                <div class="logo">
+                    <img src="../assets/img/logo.png" alt="Logo">
+                    <span>GrowTogether</span>
+                </div>
+                <div class="search-bar">
+                    <input type="text" placeholder="Search...">
+                    <i class="fa fa-search"></i>
+                </div>
+                <div class="dropdown">
+                    <!-- Dropdown content here -->
+                </div>
+            </div>
+        </div>
+        
+        <!-- Lower Navbar -->
+        <nav class="lower-navbar">
+            <div class="container">
+                <ul class="nav-links">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Portfolio</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+
+</body>
+</html>
