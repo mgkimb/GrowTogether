64785cbaa6834af9f7e4a1a9016b1af04aa9f15e
mgkimb <98755684+mgkimb@users.noreply.github.com>
2023-09-23 18:47:19 +0900

    navbar 1

--- templates/components/header.php
+++ templates/components/header.php
@@ -28,10 +28,8 @@
             <div class="container">
                 <ul class="nav-links">
                     <li><a href="#">Home</a></li>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Services</a></li>
-                    <li><a href="#">Portfolio</a></li>
-                    <li><a href="#">Contact</a></li>
+                    <li><a href="#">Plant Health</a></li>
+                    <li><a href="#">Learn Green</a></li>
                 </ul>
             </div>
         </nav>
